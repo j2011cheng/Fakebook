fe82a7c9905f7f3b533ca0a57bb97e5043ead411
Alexander Shaham <ashaham@ucsc.edu>
2021-12-01 03:47:02 -0800

    Finished the UI for desktop view's body component. Needs to be hooked to backend and tested as well as generate the listings...

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -4,73 +4,150 @@ import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import {useHistory} from 'react-router-dom';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import {Typography} from '@mui/material';
 
 /**
+ * Advertisement section of body
+ *
+ * @return {object} JSX
+ */
+function Advertisement() {
+  // if signed in, replace login button with acc badge
+  // (use from topbar when done)
+
+  const history = useHistory();
+
+  const signIn = (event) => {
+    history.push('/login');
+  };
+
+  return (
+    <Grid item xs={12}>
+      <Paper
+        sx={{
+          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          height: 150,
+        }}
+      >
+        <ListItemText sx={{mt: -1}}>
+          <Typography variant='h5'>
+            Buy and sell items locally or have
+            something new shipped from stores.
+          </Typography>
+        </ListItemText>
+        <ListItemText sx={{mt: -1}}>
+          <Typography variant='h6'>
+            Log in to get the full
+            Fakebook Marketplace experience.
+          </Typography>
+        </ListItemText>
+        <ListItem>
+          <Button
+            variant='contained'
+            onClick={signIn}
+            sx={{ml: -2, mr: 1, mt: 1, width: '50%'}}>
+            Login
+          </Button>
+          <Button
+            variant='contained'
+            fullWidth
+            sx={{ml: 1, mr: -2, mt: 1, width: '50%'}}>
+            Learn More
+          </Button>
+        </ListItem>
+      </Paper>
+    </Grid>
+  );
+}
+
+/**
+ * Category chips section of body
+ *
+ * @return {object} JSX
+ */
+function BodyCategory() {
+  // can possibly pull from filter.js/sidebar to complete
+
+  // --- Mobile ---
+  // breadcrumbs
+  // category selection (dropdown)
+  // subcategory selection (chips)
+  // search bar
+  // location filter
+  // category specific filters
+
+  // --- Desktop ---
+  // Shop by category text
+  // category chips
+
+  const handleClick = () => {
+    console.info('You clicked the Chip.');
+  };
+
+  return (
+    <Grid item xs={12}>
+      <Paper
+        sx={{
+          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          height: 100,
+        }}
+      >
+        <ListItemText sx={{mt: -1}}>
+          <Typography variant='h5'>Shop by Category</Typography>
+        </ListItemText>
+        <Stack direction='row' spacing={1}>
+          <Chip label='Vehicles' variant='outlined' onClick={handleClick}/>
+          <Chip label='Computers' variant='outlined' onClick={handleClick}/>
+        </Stack>
+      </Paper>
+    </Grid>
+  );
+}
+
+/**
+ * Show listings section of body
+ *
+ * @return {object} JSX
+ */
+function ShowListings() {
+  // listings shown
+
+  return (
+    <Grid item xs={12}>
+      <Paper
+        sx={{
+          p: 2,
+          display: 'sflex',
+          flexDirection: 'column',
+        }}>
+      </Paper>
+    </Grid>
+  );
+}
+
+/**
+ *
  * @return {object} JSX
  */
 function Body() {
-  // file for body of front end (similar to filters.js)
-
-  // buy and sell description area with login button...
-
-  // breadcrumbs
-  // category selection area with listings search
-
-  // listings shown
-  //    distance filter
-
-  // if signed in, show an account badge, otherwise show a fast login...
-  // only login button for mobile and fast login for desktop...
-
   return (
-    <Box
-      component='main'
-      sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'light' ?
-            theme.palette.grey[100] :
-            theme.palette.grey[900],
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-      }}
-    >
-      <Toolbar />
-      <Container sx={{mt: 4, mb: 4, ml: 0, mr: 0}}>
+    <Box>
+      <Toolbar/>
+      <Container sx={{mt: 4, mb: 4, ml: '50%', mr: '50%'}}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={8} lg={12}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-                // width: '100%',
-              }}
-            >
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-              }}
-            >
-            </Paper>
-          </Grid>
-          {/* Recent Orders */}
-          <Grid item xs={12}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'sflex',
-                flexDirection: 'column',
-              }}>
-            </Paper>
-          </Grid>
+          <Advertisement/>
+          <BodyCategory/>
+          <ShowListings/>
         </Grid>
       </Container>
     </Box>
