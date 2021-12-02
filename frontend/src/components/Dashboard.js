ecdca33bf9c3cd059d6ce4b7df63f3fa1e2578d5
Alexander Shaham <ashaham@ucsc.edu>
2021-12-02 03:36:37 -0800

    Completed basic sidebar mobile functionality and 100% code coverage for it.

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,37 +1,10 @@
 import * as React from 'react';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
-// import {makeStyles} from '@material-ui/core/styles';
-// import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 
 import TopBar from './TopBar';
 import SideBar from './SideBar';
 import Body from './Body';
-
-// const drawerWidth = 400;
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//   },
-//   appBar: {
-//     zIndex: theme.zIndex.drawer + 1,
-//   },
-//   drawer: {
-//     width: drawerWidth,
-//     flexShrink: 0,
-//   },
-//   drawerPaper: {
-//     width: drawerWidth,
-//   },
-//   drawerContainer: {
-//     overflow: 'auto',
-//   },
-//   content: {
-//     flexGrow: 1,
-//     padding: theme.spacing(0),
-//   },
-// }));
 
 /**
  * Dashboard Content.
@@ -42,14 +15,12 @@ function DashboardContent() {
   // probably use a 'loggedin' state to show/hide things
 
   return (
-    <ThemeProvider theme={createTheme()}>
-      <Box sx={{display: 'flex'}}>
-        <CssBaseline />
-        <TopBar/>
-        <SideBar/>
-        <Body/>
-      </Box>
-    </ThemeProvider>
+    <Box sx={{display: 'flex'}}>
+      <CssBaseline />
+      <TopBar/>
+      <SideBar/>
+      <Body/>
+    </Box>
   );
 }
 
---
+++ frontend/src/components/__tests__/SideBar.test.js
@@ -0,0 +1,45 @@
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {screen} from '@testing-library/react';
+
+import {setNarrow, setWide} from './common.js';
+import {getOnlyVisible, getNotVisible, getClickable} from './common.js';
+import SideBar from '../SideBar';
+
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+  useLocation: () => ({
+    search: '?category=2',
+  }),
+}));
+
+test('Correct Accessibility', () => {
+  render(<SideBar/>);
+  screen.getByPlaceholderText('Search Marketplace');
+  getOnlyVisible('+ Create New Listing');
+  getOnlyVisible('Categories');
+});
+
+test('Set mobile view', () => {
+  render(<SideBar/>);
+  setNarrow();
+  screen.getByPlaceholderText('Search Marketplace');
+  getOnlyVisible('+ Create New Listing');
+  getOnlyVisible('Categories');
+});
+
+test('Set mobile view then desktop view', () => {
+  render(<SideBar/>);
+  setNarrow();
+  setWide();
+  screen.getByPlaceholderText('Search Marketplace');
+  getOnlyVisible('+ Create New Listing');
+  getOnlyVisible('Categories');
+});
+
+// expect(screen.queryByText('+ Create New Listing')).not.toBeDefined();
+//   getClickable('+ Create New Listing');
