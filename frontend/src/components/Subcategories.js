f8df8aaaf6c4175008296851394b6ceb28626da5
Graham Freifeld <gifreife@ucsc.edu>
2021-12-03 23:54:36 -0800

    Close dialog on category selection

--- frontend/src/components/Subcategories.js
+++ frontend/src/components/Subcategories.js
@@ -50,7 +50,14 @@ function Subcategories() {
   const location = useLocation();
   const history = useHistory();
 
-  const setCategory = (id) => {
+  const [category, setCategory] = React.useState(
+    new URLSearchParams(location.search).get('category'));
+  React.useEffect(() => {
+    setCategory(new URLSearchParams(location.search).get('category'));
+    handleClose();
+  }, [location.search]);
+
+  const setCategoryParam = (id) => {
     return () => {
       const params = new URLSearchParams();
       params.set('category', id);
@@ -104,10 +111,9 @@ function Subcategories() {
   const [data, setData] = React.useState([]);
   React.useEffect(() => {
     const getData = async () => {
-      const params = new URLSearchParams(location.search);
       let request = '/v0/category';
-      if (params.get('category')) {
-        request += `?category=${params.get('category')}`;
+      if (category) {
+        request += `?category=${category}`;
       }
       const disp = await fetch(request, {
         method: 'GET',
@@ -132,14 +138,14 @@ function Subcategories() {
       setData(disp);
     };
     getData();
-  }, [location.search]);
+  }, [category]);
 
   const subcategory = ({name, id}) => {
     return (
       <Chip
         key={id}
         label={name}
-        onClick={setCategory(id)}
+        onClick={setCategoryParam(id)}
       />
     );
   };
